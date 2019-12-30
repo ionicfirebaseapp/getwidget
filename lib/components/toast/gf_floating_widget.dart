f892292c0bf9b3b5ad8c5baeb9b40ddb3651c474
Shravya.ckm <shravya@pietechsolution.com>
2019-12-30 16:44:45 +0530

    different positions of toast is in progress

---
+++ lib/components/toast/gf_floating_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class FloatingWidget extends StatefulWidget {
+  @override
+  _FloatingWidgetState createState() => _FloatingWidgetState();
+}
+
+class _FloatingWidgetState extends State<FloatingWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
