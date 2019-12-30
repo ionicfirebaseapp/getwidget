428fc9e09d4bbb157779bf353209c4eba1aadb65
Shravya.ckm <shravya@pietechsolution.com>
2019-12-30 10:21:41 +0530

    list tile is in progress

--- lib/components/list_tile/gf_list_tile.dart
+++ lib/components/list_tile/gf_list_tile.dart
@@ -2,28 +2,32 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ui_kit/components/avatar/gf_avatar.dart';
 
-class GFListItem extends StatelessWidget {
+class GFListTile extends StatelessWidget {
   /// The card's background color.
   final Color color;
 
   /// [GFAvatar] used to create rounded user profile
   final GFAvatar avatar;
 
-  /// The title to display inside the [GFListItem]. see [Text]
+  /// The title to display inside the [GFListTile]. see [Text]
   final Widget title;
 
-  /// The subTitle to display inside the [GFListItem]. see [Text]
+  /// The subTitle to display inside the [GFListTile]. see [Text]
   final Widget subTitle;
 
-  /// The icon to display inside the [GFListItem]. see [Icon]
+  /// The descriprion to display inside the [GFListTile]. see [Text]
+  final Widget description;
+
+  /// The icon to display inside the [GFListTile]. see [Icon]
   final Widget icon;
 
-  const GFListItem(
+  const GFListTile(
       {Key key,
       this.color,
       this.avatar,
       this.title,
       this.subTitle,
+        this.description,
       this.icon})
       : super(key: key);
 
@@ -32,7 +36,13 @@ class GFListItem extends StatelessWidget {
     return ListTile(
       leading: avatar,
       title: title,
-      subtitle: subTitle,
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          subTitle ?? Container(),
+          description ?? Container()
+          ],
+      ),
       trailing: icon,
     );
   }
--- lib/components/slider/gf_slider.dart
+++ lib/components/slider/gf_slider.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'dart:ffi';
+//import 'dart:ffi';
 import 'package:flutter/material.dart';
 
 List<T> map<T>(List list, Function handler) {
