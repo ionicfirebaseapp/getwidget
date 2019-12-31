b1b0e5f8a15ea11bed1407bbd704a0a049618e27
deepikahr <deepika@pietechsolution.com>
2019-12-31 15:09:07 +0530

    gf button elevation fixed

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,6 +30,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:ui_kit/size/gf_size.dart';
 import 'package:ui_kit/position/gf_position.dart';
 import 'package:ui_kit/components/tabs/gf_tabs.dart';
+import 'package:ui_kit/components/slider/carousel.dart';
 
 final List<String> imageList = [
   "https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg",
@@ -78,102 +79,116 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
 
-            GFSlider(
+            Carousel(
+                rowCount: 3,
+                children: [
+                  'Item 1',
+                  'Item 2',
+                  'Item 3',
+                  'Item 4',
+                  'Item 5',
+                  'Item 6',
+                ].map((String itemText) {
+                  return DefaultCarouselItem(itemText);
+                }).toList()
+            ),
+
+//            GFSlider(
 //              pagerSize: 12.0,
 //              activeIndicator: Colors.pink,
 //              passiveIndicator: Colors.pink.withOpacity(0.4),
-              viewportFraction: 0.9,
-              aspectRatio: 2.0,
-//            autoPlay: true,
-              enlargeMainPage: true,
-              pagination: true,
-              items: imageList.map(
-                (url) {
-                  return Container(
-                    margin: EdgeInsets.all(5.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                      child: Image.network(
-                        url,
-                        fit: BoxFit.cover,
-                        width: 1000.0,
-                      ),
-                    ),
-                  );
-                },
-              ).toList(),
-              onPageChanged: (index) {
-                setState(() {
-                  index;
-                });
-              },
-            ),
+//              viewportFraction: 0.9,
+//              aspectRatio: 2.0,
+////            autoPlay: true,
+//              enlargeMainPage: true,
+//              pagination: true,
+//              items: imageList.map(
+//                (url) {
+//                  return Container(
+//                    margin: EdgeInsets.all(5.0),
+//                    child: ClipRRect(
+//                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+//                      child: Image.network(
+//                        url,
+//                        fit: BoxFit.cover,
+//                        width: 1000.0
+//                      ),
+//                    ),
+//                  );
+//                },
+//              ).toList(),
+//              onPageChanged: (index) {
+//                setState(() {
+//                  index;
+//                });
+//              },
+//            ),
 
-            GFButton(
-              color: Colors.orange,
-              onPressed: null,
-              child: Text("share"),
-              type: GFType.outline,
-              shape: GFShape.pills,
-//              buttonBoxShadow: true,
-//              boxShadow: BoxShadow(
-//                color: Colors.pink,
-//                blurRadius: 1.5,
-//                spreadRadius: 2.0,
-//                offset: Offset.zero,
+//            GFButton(
+//              color: Colors.orange,
+//              onPressed: null,
+//              child: Text("share"),
+//              type: GFType.outline,
+//              shape: GFShape.pills,
+////              buttonBoxShadow: true,
+////              boxShadow: BoxShadow(
+////                color: Colors.pink,
+////                blurRadius: 1.5,
+////                spreadRadius: 2.0,
+////                offset: Offset.zero,
+////              ),
+//            ),
+
+//            GFTabs(
+//              initialIndex: 0,
+//              length: 3,
+//              tabs: <Widget>[
+//                GFButton(
+//                  onPressed: null,
+//                  child: Text("share"),
+//                  icon: Icon(Icons.share),
+//                  buttonBoxShadow: true,
+//                ),
+//                Tab(
+//                  icon: Icon(Icons.error),
+//                  child: Text(
+//                    "Orders",
+//                  ),
+//                ),
+//                Tab(
+//                  child: Text(
+//                    "Pastry",
+//                  ),
+//                ),
+//              ],
+//              tabBarView: TabBarView(
+//                children: <Widget>[
+//                  Container(
+//                    color: Colors.red,
+//                    child: Column(
+//                      mainAxisAlignment: MainAxisAlignment.center,
+//                      crossAxisAlignment: CrossAxisAlignment.center,
+//                      children: <Widget>[
+//                        RawMaterialButton(
+//                          onPressed: null,
+//                          child: Text("fv"),
+//                        ),
+//                        FlatButton(onPressed: null, child: Text("cds")),
+//                        Icon(Icons.directions_railway),
+//                        GFButton(
+//                          onPressed: null,
+//                          child: Text("share"),
+//                          icon: Icon(Icons.share),
+//                          shape: GFShape.pills,
+//                          type: GFType.transparent,
+//                        ),
+//                      ],
+//                    ),
+//                  ),
+//                  Icon(Icons.directions_car),
+//                  Icon(Icons.directions_transit),
+//                ],
 //              ),
-            ),
-
-            GFTabs(
-              initialIndex: 0,
-              length: 3,
-              tabs: <Widget>[
-                GFButton(
-                  onPressed: null,
-                  child: Text("share"),
-                  icon: Icon(Icons.share),
-                  buttonBoxShadow: true,
-                ),
-                Tab(
-                  icon: Icon(Icons.error),
-                  child: Text(
-                    "Orders",
-                  ),
-                ),
-                Tab(
-                  child: Text(
-                    "Pastry",
-                  ),
-                ),
-              ],
-              tabBarView: TabBarView(
-                children: <Widget>[
-                  Container(
-                    color: Colors.red,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        RawMaterialButton(
-                          onPressed: null,
-                          child: Text("fv"),
-                        ),
-                        FlatButton(onPressed: null, child: Text("cds")),
-                        Icon(Icons.directions_railway),
-                        GFButton(
-                          onPressed: null,
-                          child: Text("share"),
-                          icon: Icon(Icons.share),
-                          shape: GFShape.pills,
-                          type: GFType.transparent,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Icon(Icons.directions_car),
-                  Icon(Icons.directions_transit),
-                ],
-              ),
 //              indicatorColor: Colors.teal,
 //              indicatorSize: TabBarIndicatorSize.label,
 //              labelColor: Colors.lightGreen,
@@ -190,7 +205,7 @@ class _MyHomePageState extends State<MyHomePage> {
 //                color: Colors.black,
 //                fontFamily: 'OpenSansBold',
 //              ),
-            ),
+//            ),
 
 //              GFSlider(
 //                autoPlay: true,
@@ -217,77 +232,80 @@ class _MyHomePageState extends State<MyHomePage> {
 //                  });
 //                },
 //              ),
-            GFCard(
-              boxFit: BoxFit.cover,
-              colorFilter: new ColorFilter.mode(
-                  Colors.black.withOpacity(0.67), BlendMode.darken),
-              image: Image.asset("lib/assets/food.jpeg"),
-//              imageOverlay: AssetImage("lib/assets/food.jpeg"),
-              titlePosition: GFPosition.end,
-              title: GFListTile(
-                avatar: GFAvatar(
-                  child: Text("tb"),
-                ),
-                title: Text(
-                  'title',
-                  style: TextStyle(color: Colors.grey),
-                ),
-                subTitle: Text(
-                  'subtitle',
-                  style: TextStyle(color: Colors.grey),
-                ),
-                icon: GFIconButton(
-                  onPressed: null,
-                  icon: Icon(Icons.favorite_border),
-                  type: GFType.transparent,
-                ),
-              ),
-              content: Text(
-                "Flutter "
-                "Flutter is Google's mobile UI framework for crafting"
-                " high-quality native interfaces on iOS and Android in "
-                "Flutter ",
-                style: TextStyle(color: Colors.grey),
-              ),
-              buttonBar: GFButtonBar(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  GFButton(
-                    onPressed: null,
-                    child: Text("favorite"),
-                    icon: Icon(Icons.favorite_border),
-                    type: GFType.transparent,
-                  ),
-                  GFButton(
-                    onPressed: null,
-                    child: Text("share"),
-                    icon: Icon(Icons.share),
-                    type: GFType.outline,
-                  ),
-                ],
-              ),
-            ),
-            GFButtonBar(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
+//            GFCard(
+//              boxFit: BoxFit.cover,
+//              colorFilter: new ColorFilter.mode(
+//                  Colors.black.withOpacity(0.67), BlendMode.darken),
+//              image: Image.asset("lib/assets/food.jpeg"),
+////              imageOverlay: AssetImage("lib/assets/food.jpeg"),
+//              titlePosition: GFPosition.end,
+//              title: GFListTile(
+//                avatar: GFAvatar(
+//                  child: Text("tb"),
+//                ),
+//                title: Text(
+//                  'title',
+//                  style: TextStyle(color: Colors.grey),
+//                ),
+//                subTitle: Text(
+//                  'subtitle',
+//                  style: TextStyle(color: Colors.grey),
+//                ),
+//                icon: GFIconButton(
+//                  onPressed: null,
+//                  icon: Icon(Icons.favorite_border),
+//                  type: GFType.transparent,
+//                ),
+//              ),
+//              content: Text(
+//                "Flutter "
+//                "Flutter is Google's mobile UI framework for crafting"
+//                " high-quality native interfaces on iOS and Android in "
+//                "Flutter ",
+//                style: TextStyle(color: Colors.grey),
+//              ),
+//              buttonBar: GFButtonBar(
+//                mainAxisSize: MainAxisSize.min,
+//                children: <Widget>[
+//                  GFButton(
+//                    onPressed: null,
+//                    child: Text("favorite"),
+//                    icon: Icon(Icons.favorite_border),
+//                    type: GFType.transparent,
+//                  ),
+//                  GFButton(
+//                    onPressed: null,
+//                    child: Text("share"),
+//                    icon: Icon(Icons.share),
+//                    type: GFType.outline,
+//                  ),
+//                ],
+//              ),
+//            ),
+//            GFButtonBar(
+//              mainAxisSize: MainAxisSize.min,
+//              children: <Widget>[
+//                GFButton(
+//                  onPressed: null,
+//                  child: Text("like"),
+//                  icon: Icon(Icons.favorite_border),
+//                  type: GFType.transparent,
+//                ),
+//                GFButton(
+//                  onPressed: null,
+//                  child: Text("comment"),
+//                ),
                 GFButton(
-                  onPressed: null,
-                  child: Text("like"),
-                  icon: Icon(Icons.favorite_border),
-                  type: GFType.transparent,
-                ),
-                GFButton(
-                  onPressed: null,
-                  child: Text("comment"),
-                ),
-                GFButton(
-                  onPressed: null,
+                  color: Colors.teal,
+                  onPressed: (){},
                   child: Text("share"),
                   icon: Icon(Icons.share),
                   type: GFType.outline,
                 ),
-              ],
-            ),
+//              ],
+//            ),
+
+
 //            GFListItem(
 //              avatar: GFAvatar(
 //                child: Text("tb"),
---
+++ lib/components/slider/carousel.dart
@@ -0,0 +1,279 @@
+library carousel;
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+/// Signature for when a pointer has contacted the screen and has begun to move.
+///
+/// The `details` object provides the position of the touch when it first
+/// touched the surface.
+typedef CarouselDragStartCallback = void Function(DragStartDetails details);
+
+/// Signature for when a pointer that is in contact with the screen and moving
+/// has moved again.
+///
+/// The `details` object provides the position of the touch and the distance it
+/// has travelled since the last update.
+typedef CarouselDragCallback = void Function(DragUpdateDetails details);
+
+/// Signature for when a pointer that was previously in contact with the screen
+/// and moving is no longer in contact with the screen.
+///
+/// The velocity at which the pointer was moving when it stopped contacting
+/// the screen is available in the `details`.
+typedef CarouselDragEndCallback = void Function(DragEndDetails details);
+
+/// A widget that show draggable cells with animation.
+///
+/// Set [rowCount] of visible cells
+///
+/// Set drag handlers [onDragStart], [onDrag], [onDragEnd]
+///
+/// Set left/right arrows [leftArrow], [rightArrow]
+class Carousel extends StatefulWidget {
+  /// Count of visible cells
+  int rowCount;
+
+  /// Customize widget by left/right arrows
+  CarouselArrow leftArrow, rightArrow;
+
+  List<Widget> children;
+
+  /// Signature for when a pointer has contacted the screen and has begun to move.
+  CarouselDragStartCallback onDragStart;
+
+  /// Signature for when a pointer that is in contact with the screen and moving
+  /// has moved again.
+  CarouselDragCallback onDrag;
+
+  /// Signature for when a pointer that was previously in contact with the screen
+  /// and moving is no longer in contact with the screen.
+  CarouselDragEndCallback onDragEnd;
+
+  Carousel({
+    this.rowCount,
+    this.children,
+    this.leftArrow,
+    this.rightArrow,
+    this.onDragStart,
+    this.onDrag,
+    this.onDragEnd
+  });
+
+  @override
+  _CarouselState createState() => new _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> with TickerProviderStateMixin {
+  /// In milliseconds
+  static final int DRAG_ANIMATION_DURATION = 1000;
+
+  /// In milliseconds
+  static final int SHIFT_ANIMATION_DURATION = 300;
+
+  /// Size of cell
+  double size = 0;
+
+  /// Width of cells container
+  double width = 0;
+
+  AnimationController animationController;
+
+  /// Shift of cells container
+  double offset;
+
+  @override
+  void initState() {
+    super.initState();
+
+    this.offset = 0;
+
+    this.animationController = AnimationController(
+        duration: new Duration(milliseconds: DRAG_ANIMATION_DURATION),
+        vsync: this
+    );
+
+    new Future.delayed(Duration.zero, () {
+      this.setState(() {
+        /// Calculate cells container width
+        CarouselArrow leftArrow = widget.leftArrow;
+        CarouselArrow rightArrow = widget.rightArrow;
+
+        double width = MediaQuery.of(context).size.width;
+
+        if (leftArrow != null && leftArrow.constraints != null) {
+          width -= leftArrow.constraints.constrainWidth();
+        }
+
+        if (rightArrow != null && rightArrow.constraints != null) {
+          width -= rightArrow.constraints.constrainWidth();
+        }
+
+        this.width = width;
+        this.size = this.width / widget.rowCount;
+      });
+    });
+  }
+
+  double calculateOffset(double shift) {
+    double offset = this.offset + shift;
+    double rightLimit = this.size * (widget.children.length - widget.rowCount);
+
+    /// Check cells container limits
+    if (offset > 0) {
+      offset = 0;
+    } else if (offset < -rightLimit) {
+      offset = -rightLimit;
+    }
+
+    return offset;
+  }
+
+  onDragStart(DragStartDetails details) {
+    this.animationController.stop();
+    widget.onDragStart != null ? widget.onDragStart(details) : null;
+  }
+
+  onDrag(DragUpdateDetails details) {
+    setState(() {
+      this.offset = this.calculateOffset(3 * details.delta.dx);
+    });
+
+    widget.onDrag != null ? widget.onDrag(details) : null;
+  }
+
+  onDragEnd(DragEndDetails details) {
+    double dx = details.velocity.pixelsPerSecond.dx;
+
+    if (dx == 0) {
+      return this.runShiftAnimation();
+    }
+
+    this.animationController = new AnimationController(
+        duration: new Duration(milliseconds: DRAG_ANIMATION_DURATION),
+        vsync: this
+    );
+
+    Tween tween = new Tween<double>(
+        begin: this.offset,
+        end: this.calculateOffset(0.5 * dx)
+    );
+
+    Animation animation = tween.animate(
+        new CurvedAnimation(
+          parent: this.animationController,
+          curve: Curves.easeOut,
+        )
+    );
+
+    animation.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        this.runShiftAnimation();
+      }
+    });
+
+    animation.addListener(() {
+      setState(() {
+        this.offset = animation.value;
+      });
+    });
+
+    this.animationController.forward();
+    widget.onDragEnd != null ? widget.onDragEnd(details) : null;
+  }
+
+  runShiftAnimation() {
+    double beginAnimation = this.offset;
+    double endAnimation =
+        this.size * (this.offset / this.size).round().toDouble();
+
+    this.animationController = new AnimationController(
+        duration: new Duration(milliseconds: SHIFT_ANIMATION_DURATION),
+        vsync: this
+    );
+    Tween tween = new Tween<double>(begin: beginAnimation, end: endAnimation);
+    Animation animation = tween.animate(this.animationController);
+
+    animation.addListener(() {
+      setState(() {
+        this.offset = animation.value;
+      });
+    });
+
+    this.animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onHorizontalDragStart: this.onDragStart,
+      onHorizontalDragUpdate: this.onDrag,
+      onHorizontalDragEnd: this.onDragEnd,
+      child: Container(
+        width: double.infinity,
+        height: this.size,
+        child: Container(
+          width: this.width,
+          height: this.size,
+          child: Stack(
+              children: [
+                Positioned(
+                  left: this.offset,
+                  child: Row(
+                    children: widget.children.map((child) {
+                      return Container(
+                        width: this.size,
+                        height: this.size,
+                        child: child,
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ]
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CarouselArrow extends Container {
+
+  CarouselArrow({ Key key, double width, Widget child })
+      : super(key: key, width: width, child: child);
+}
+
+
+class DefaultCarouselItem extends StatelessWidget {
+  String text;
+
+  DefaultCarouselItem(String text) {
+    this.text = text;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(6.0),
+      alignment: Alignment.center,
+      decoration: new BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(6.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              this.text,
+              style: TextStyle(
+                  color: Colors.white
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
