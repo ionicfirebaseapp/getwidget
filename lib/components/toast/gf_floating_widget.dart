9f85445c33341d6490f2ca57759a83e6d50833d2
Shravya.ckm <shravya@pietechsolution.com>
2019-12-31 10:46:50 +0530

    comments added to gfFloatingWidget

--- lib/components/toast/gf_floating_widget.dart
+++ lib/components/toast/gf_floating_widget.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:ui_kit/components/toast/gf_toast.dart';
 
-
-
 class GFFloatingWidget extends StatefulWidget {
+  const GFFloatingWidget(
+      {Key key,
+      this.child,
+      this.horizontalPosition,
+      this.verticalPosition,
+      this.body})
+      : super(key: key);
 
-  const GFFloatingWidget({
-    Key key,
-    this.child,
-    this.horizontalPosition,
-    this.verticalPosition,
-    this.body
-  }) : super(key: key);
-
-
+  ///child of  type [Widget] which floats across the body based on horizontal and vertical position
   final Widget child;
 
+  ///body of type [Widget] which is same as Scaffold's body
   final Widget body;
 
+  /// horizontalPosition of type [double] which  aligns the child horizontally across the body
   final double horizontalPosition;
 
-
+  /// verticalPosition of type [double] which  aligns the child vertically across the body
   final double verticalPosition;
 
   @override
@@ -34,17 +33,18 @@ class _GFFloatingWidgetState extends State<GFFloatingWidget> {
     var screenHeight = MediaQuery.of(context).size.height;
     return Stack(
       children: <Widget>[
-      Container(
-         child: widget.body ?? Container(),
-      ),
+        Container(
+          child: widget.body ?? Container(),
+        ),
         Positioned(
-          top: widget.horizontalPosition != null ? widget.horizontalPosition : 0,
-          left: widget.verticalPosition  != null ? widget.verticalPosition : 0,
+            top: widget.horizontalPosition != null
+                ? widget.horizontalPosition
+                : 0,
+            left: widget.verticalPosition != null ? widget.verticalPosition : 0,
             child: Container(
               width: MediaQuery.of(context).size.width,
-              child:  widget.child ?? Container(),
-            )
-        ),
+              child: widget.child ?? Container(),
+            )),
       ],
     );
   }
