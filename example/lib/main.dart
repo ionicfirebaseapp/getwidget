f8e8fd30e350db3952c6dcb58b5bc43618ca8127
deepikahr <deepika@pietechsolution.com>
2019-12-31 11:03:38 +0530

    transparent button and box shadow in outline completed

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -67,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange,
+      backgroundColor: Colors.cyanAccent,
       appBar: AppBar(
         title: Text(widget.title),
       ),
@@ -76,95 +76,51 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-//            Container(
-//              height: 300.0,
-//              child: DefaultTabController(
-//                length: 2,
-//                child: Scaffold(
-//                  appBar: AppBar(
-//                    bottom: TabBar(
-//                      indicator: UnderlineTabIndicator(
-//                        borderSide: BorderSide(color: Color(0xDD613896), width: 2.0),
-//                        insets: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 4.0),
-//                      ),
-//                      tabs: [
-//                        Tab(icon: Icon(Icons.directions_car), text: "Non persistent",),
-//                        Tab(icon: Icon(Icons.directions_transit), text: "Persistent"),
-//                      ],
-//                    ),
-//                    title: Text('Persistent Tab Demo'),
-//                  ),
-//                  body: TabBarView(
-//                    children: [
-//                      Text("cdx"),
-//                      Text("cdv")
-//                    ],
-//                  ),
-//                ),
-//              ),
-//            ),
 
-//            GFSlider(
-////              pagerSize: 12.0,
-////              activeIndicator: Colors.pink,
-////              passiveIndicator: Colors.pink.withOpacity(0.4),
-//              viewportFraction: 0.9,
-//              aspectRatio: 2.0,
-////            autoPlay: true,
-//              enlargeMainPage: true,
-//              pagination: true,
-//              items: imageList.map(
-//                (url) {
-//                  return Container(
-//                    margin: EdgeInsets.all(5.0),
-//                    child: ClipRRect(
-//                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
-//                      child: Image.network(
-//                        url,
-//                        fit: BoxFit.cover,
-//                        width: 1000.0,
-//                      ),
-//                    ),
-//                  );
-//                },
-//              ).toList(),
-//              onPageChanged: (index) {
-//                setState(() {
-//                  index;
-//                });
-//              },
-//            ),
+            GFSlider(
+//              pagerSize: 12.0,
+//              activeIndicator: Colors.pink,
+//              passiveIndicator: Colors.pink.withOpacity(0.4),
+              viewportFraction: 0.9,
+              aspectRatio: 2.0,
+//            autoPlay: true,
+              enlargeMainPage: true,
+              pagination: true,
+              items: imageList.map(
+                (url) {
+                  return Container(
+                    margin: EdgeInsets.all(5.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                      child: Image.network(
+                        url,
+                        fit: BoxFit.cover,
+                        width: 1000.0,
+                      ),
+                    ),
+                  );
+                },
+              ).toList(),
+              onPageChanged: (index) {
+                setState(() {
+                  index;
+                });
+              },
+            ),
 
-            RawMaterialButton(onPressed: null, child: Text("fv"),),
-            FlatButton(onPressed: null, child: Text("cds")),
-
-//            Container(
-//              decoration: BoxDecoration(
-//                color: Colors.teal,
-//                  boxShadow: [BoxShadow(
-//                      color: Colors.pink,
-//                      blurRadius: 1.5,
-//                      spreadRadius: 2.0,
-//                      offset: Offset.zero,
-//                    )
-//                  ]
-//              ),
-//                child: OutlineButton(child: Text("Fvd"),)),
-//
-//
             GFButton(
               color: Colors.orange,
               onPressed: null,
               child: Text("share"),
               type: GFType.outline,
               shape: GFShape.pills,
-              buttonBoxShadow: true,
-              boxShadow: BoxShadow(
-                color: Colors.pink.withOpacity(0.4),
-                blurRadius: 1.5,
-                spreadRadius: 2.0,
-                offset: Offset.zero,
-              ),
+//              buttonBoxShadow: true,
+//              boxShadow: BoxShadow(
+//                color: Colors.pink,
+//                blurRadius: 1.5,
+//                spreadRadius: 2.0,
+//                offset: Offset.zero,
+//              ),
             ),
 
             GFTabs(
