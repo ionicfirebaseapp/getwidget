43d86cc4bafd0c0cf54717eb1abb07c9e7c8b70c
Sandip Kakadiya <Sandip@pietechsolution.com>
2020-01-30 16:26:03 +0530

    drawer code formatted

--- lib/components/drawer/gf_drawer_header.dart
+++ lib/components/drawer/gf_drawer_header.dart
@@ -5,71 +5,70 @@ import 'package:getflutter/components/button/gf_icon_button.dart';
 import 'package:getflutter/getflutter.dart';
 
 class GFDrawerHeaderPictures extends StatelessWidget {
-  const GFDrawerHeaderPictures(
-      {Key key,
-      this.currentAccountPicture,
-      this.otherAccountsPictures,
-      this.closeButton})
-      : super(key: key);
+  const GFDrawerHeaderPictures({
+    Key key,
+    this.currentAccountPicture,
+    this.otherAccountsPictures,
+    this.closeButton,
+  }) : super(key: key);
 
   final Widget currentAccountPicture;
+
   final List<Widget> otherAccountsPictures;
 
   /// widget onTap drawer get closed
   final Widget closeButton;
 
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        closeButton == null
-            ? GFIconButton(
-                icon: Icon(
-                  Icons.close,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                type: GFButtonType.transparent,
-              )
-            : closeButton,
-        PositionedDirectional(
-          top: 0.0,
-          end: 0.0,
-          child: Row(
-            children: (otherAccountsPictures ?? <Widget>[])
-                .take(3)
-                .map<Widget>((Widget picture) {
-              return Padding(
-                padding: const EdgeInsetsDirectional.only(start: 8.0),
-                child: Semantics(
-                  container: true,
-                  child: Container(
-                    padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
-                    width: 48.0,
-                    height: 48.0,
-                    child: picture,
+  Widget build(BuildContext context) => Stack(
+        children: <Widget>[
+          closeButton == null
+              ? GFIconButton(
+                  icon: Icon(
+                    Icons.close,
+                    color: Colors.white,
                   ),
-                ),
-              );
-            }).toList(),
-          ),
-        ),
-        Positioned(
-          top: 40.0,
-          child: Semantics(
-            explicitChildNodes: true,
-            child: SizedBox(
-              width: 72.0,
-              height: 72.0,
-              child: currentAccountPicture,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  type: GFButtonType.transparent,
+                )
+              : closeButton,
+          PositionedDirectional(
+            top: 0,
+            end: 0,
+            child: Row(
+              children: (otherAccountsPictures ?? <Widget>[])
+                  .take(3)
+                  .map<Widget>((Widget picture) {
+                return Padding(
+                  padding: const EdgeInsetsDirectional.only(start: 8),
+                  child: Semantics(
+                    container: true,
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 8, bottom: 8),
+                      width: 48,
+                      height: 48,
+                      child: picture,
+                    ),
+                  ),
+                );
+              }).toList(),
             ),
           ),
-        ),
-      ],
-    );
-  }
+          Positioned(
+            top: 40,
+            child: Semantics(
+              explicitChildNodes: true,
+              child: SizedBox(
+                width: 72,
+                height: 72,
+                child: currentAccountPicture,
+              ),
+            ),
+          ),
+        ],
+      );
 }
 
 /// A material design [Drawer] header that identifies the app's user.
@@ -80,17 +79,17 @@ class GFDrawerHeader extends StatefulWidget {
   /// Creates a material design drawer header.
   ///
   /// Requires one of its ancestors to be a [Material] widget.
-  const GFDrawerHeader(
-      {Key key,
-      this.decoration,
-      this.margin = const EdgeInsets.only(bottom: 8.0),
-      this.currentAccountPicture,
-      this.otherAccountsPictures,
-      this.child,
-      this.duration = const Duration(milliseconds: 250),
-      this.curve = Curves.fastOutSlowIn,
-      this.closeButton})
-      : super(key: key);
+  const GFDrawerHeader({
+    Key key,
+    this.decoration,
+    this.margin = const EdgeInsets.only(bottom: 8),
+    this.currentAccountPicture,
+    this.otherAccountsPictures,
+    this.child,
+    this.duration = const Duration(milliseconds: 250),
+    this.curve = Curves.fastOutSlowIn,
+    this.closeButton,
+  }) : super(key: key);
 
   /// The header's background. If decoration is null then a [BoxDecoration]
   /// with its background color set to the current theme's primaryColor is used.
@@ -146,7 +145,7 @@ class _GFDrawerHeaderState extends State<GFDrawerHeader> {
               color: Theme.of(context).primaryColor,
             ),
         margin: widget.margin,
-        padding: const EdgeInsetsDirectional.only(top: 16.0, start: 16.0),
+        padding: const EdgeInsetsDirectional.only(top: 16, start: 16),
         child: SafeArea(
           bottom: false,
           child: Column(
@@ -155,7 +154,7 @@ class _GFDrawerHeaderState extends State<GFDrawerHeader> {
             children: <Widget>[
               Expanded(
                 child: Padding(
-                  padding: const EdgeInsetsDirectional.only(end: 16.0),
+                  padding: const EdgeInsetsDirectional.only(end: 16),
                   child: GFDrawerHeaderPictures(
                     currentAccountPicture: widget.currentAccountPicture,
                     otherAccountsPictures: widget.otherAccountsPictures,
@@ -164,7 +163,7 @@ class _GFDrawerHeaderState extends State<GFDrawerHeader> {
                 ),
               ),
               AnimatedContainer(
-                padding: EdgeInsets.only(bottom: 16.0),
+                padding: const EdgeInsets.only(bottom: 16),
                 duration: widget.duration,
                 curve: widget.curve,
                 child: widget.child,
