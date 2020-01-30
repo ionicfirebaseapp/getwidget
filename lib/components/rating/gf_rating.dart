a53a6352a1dcbc66b547f6fa5079617709f1ebdb
deepikahr <deepika@pietechsolution.com>
2020-01-30 17:45:47 +0530

    rating component created

---
+++ lib/components/rating/gf_rating.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GFRating extends StatefulWidget {
+  @override
+  _GFRatingState createState() => _GFRatingState();
+}
+
+class _GFRatingState extends State<GFRating> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
