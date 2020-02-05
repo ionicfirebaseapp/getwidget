ddce240ed431e1ec586830e5cbcfec28704937b5
deepikahr <deepika@pietechsolution.com>
2020-02-05 10:59:00 +0530

    gfslidable component added

---
+++ lib/components/slidable/gf_slidable.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GFSlidable extends StatefulWidget {
+  @override
+  _GFSlidableState createState() => _GFSlidableState();
+}
+
+class _GFSlidableState extends State<GFSlidable> {
+  @override
+  Widget build(BuildContext context) =>
+      Container();
+}
