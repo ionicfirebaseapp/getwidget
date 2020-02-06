76f8d9d8f4ece05969819791917689672fa76c38
deepikahr <deepika@pietechsolution.com>
2020-02-06 11:40:42 +0530

    working on gfslidable ondrag start

--- lib/components/slidable/gf_slidable.dart
+++ lib/components/slidable/gf_slidable.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getflutter/components/button/gf_icon_button.dart';
 
 class GFSlidable extends StatefulWidget {
   const GFSlidable({
@@ -13,9 +14,30 @@ class GFSlidable extends StatefulWidget {
 }
 
 class _GFSlidableState extends State<GFSlidable> {
+
+bool showAction = false;
+  
+  void _onDragStart(DragEndDetails details) {
+    if (details.primaryVelocity == 0)
+      return; // user have just tapped on screen (no dragging)
+
+    if (details.primaryVelocity.compareTo(0) == 1) {
+       setState(() {
+         showAction = true;
+       });
+    }
+  }
+  
+  Widget action = Row(
+    children: <Widget>[
+      GFIconButton(icon: Icon(Icons.favorite), onPressed: null)
+    ],
+  );
+
   @override
   Widget build(BuildContext context) =>
-      Container(
-        child: widget.child,
+      GestureDetector(
+        onHorizontalDragEnd: (DragEndDetails details) => _onDragStart(details),
+  child: !showAction ?  widget.child : action,
       );
 }
