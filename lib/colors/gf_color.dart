146e66a74a996789155dc86ac26297e8eb67f27c
Sandip Kakadiya <Sandip@pietechsolution.com>
2020-02-11 11:37:40 +0530

    GFColor file changes

--- lib/colors/gf_color.dart
+++ lib/colors/gf_color.dart
@@ -1,20 +1,5 @@
 import 'package:flutter/material.dart';
 
-enum GFColor {
-  primary,
-  secondary,
-  success,
-  info,
-  warning,
-  danger,
-  focus,
-  alt,
-  light,
-  dark,
-  white,
-  transparent
-}
-
 class GFColors {
   static const Color PRIMARY = Color(0xff3880FF);
   static const Color SECONDARY = Color(0xffAA66CC);
@@ -28,53 +13,4 @@ class GFColors {
   static const Color FOCUS = Color(0xff434054);
   static const Color ALT = Color(0xff794c8a);
   static const Color TRANSPARENT = Colors.transparent;
-
-  /// Pass [GFColor] or [Color]
-  static Color getGFColor(dynamic color) {
-    if (color is Color) {
-      return color;
-    } else {
-      switch (color) {
-        case GFColor.primary:
-          return PRIMARY;
-          break;
-        case GFColor.secondary:
-          return SECONDARY;
-          break;
-        case GFColor.success:
-          return SUCCESS;
-          break;
-        case GFColor.warning:
-          return WARNING;
-          break;
-        case GFColor.info:
-          return INFO;
-          break;
-        case GFColor.danger:
-          return DANGER;
-          break;
-        case GFColor.focus:
-          return FOCUS;
-          break;
-        case GFColor.alt:
-          return ALT;
-          break;
-        case GFColor.light:
-          return LIGHT;
-          break;
-        case GFColor.dark:
-          return DARK;
-          break;
-        case GFColor.white:
-          return WHITE;
-          break;
-        case GFColor.transparent:
-          return TRANSPARENT;
-          break;
-        default:
-          return null;
-          break;
-      }
-    }
-  }
 }
