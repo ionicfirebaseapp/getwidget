3ed19f24ef8b2173b3a46300afd8de5d263b9a43
srinivas <srinivas@droid5.com>
2020-09-10 14:56:18 +0530

    completed bottom view for gf intro slider

--- lib/components/intro_screen/gf__intro_bottom_navigation.dart
+++ lib/components/intro_screen/gf__intro_bottom_navigation.dart
@@ -1,27 +1,35 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:getwidget/components/loader/gf_loader.dart';
-import 'package:getwidget/getwidget.dart';
 
 class GFIntroBottomNavigation extends StatelessWidget {
-  const GFIntroBottomNavigation(
-      {Key key,
-      this.rightText = 'NEXT',
-      this.pageNumber = 0,
-      this.onNext,
-      this.showDivider = true,
-      this.dividerColor = Colors.grey,
-      this.dividerHeight = 1,
-      this.dividerThickness = 0.0,
-      this.child,
-      this.padding = const EdgeInsets.all(8),
-      this.margin = const EdgeInsets.all(8),
-      this.pagesCount = 4,
-      this.skipText = 'SKIP',
-      this.onSkipTap,
-      this.skipWidget,
-      this.nextWidget,
-      this.pageCount = 1})
-      : super(key: key);
+  const GFIntroBottomNavigation({
+    Key key,
+    this.rightText = 'NEXT',
+    this.pageNumber = 0,
+    this.onNext,
+    this.showDivider = true,
+    this.dividerColor = Colors.grey,
+    this.dividerHeight = 1,
+    this.dividerThickness = 0.0,
+    this.child,
+    this.padding = const EdgeInsets.all(8),
+    this.margin = const EdgeInsets.all(8),
+    this.pagesCount = 3,
+    this.skipText = 'SKIP',
+    this.onSkipTap,
+    this.skipWidget,
+    this.rightWidget,
+    this.dotShape,
+    this.defaultColor,
+    this.activeColor,
+    this.dotHeight,
+    this.dotWidth,
+    this.dotMargin,
+    this.skipStyle,
+    this.rightStyle,
+    this.onDoneTap,
+    this.doneText = 'GO',
+  }) : super(key: key);
 
   final String rightText;
   final int pageNumber;
@@ -34,73 +42,117 @@ class GFIntroBottomNavigation extends StatelessWidget {
   final int pagesCount;
   final String skipText;
   final VoidCallback onSkipTap;
+  final VoidCallback onDoneTap;
   final EdgeInsets padding;
   final EdgeInsets margin;
   final Widget skipWidget;
-  final Widget nextWidget;
-  final int pageCount;
+  final Widget rightWidget;
+  final TextStyle skipStyle;
+  final TextStyle rightStyle;
+  final String doneText;
+
+  ///dot
+  final BoxShape dotShape;
+  final Color defaultColor;
+  final Color activeColor;
+  final double dotHeight;
+  final double dotWidth;
+  final EdgeInsets dotMargin;
 
   @override
   Widget build(BuildContext context) => Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            showDivider
-                ? Divider(
-                    height: dividerHeight,
-                    thickness: dividerThickness,
-                    color: dividerColor,
-                  )
-                : Container(),
-            Container(
-              padding: padding,
-              margin: margin,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  GestureDetector(
-                    behavior: HitTestBehavior.translucent,
-                    child: Padding(
-                        padding: const EdgeInsets.only(
-                          top: 8,
-                          bottom: 8,
-                          left: 24,
-                          right: 32,
-                        ),
-                        child: skipWidget ?? Text(skipText)),
-                    onTap: onSkipTap,
-                  ),
-                  SizedBox(
-                    height: 40,
-                    width: 100,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: pageCount,
-                      itemBuilder: (context, index) => Container(
-                        width: 12,
-                        height: 12,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: pageCount == 0
-                              ? const Color(0xff3adecb)
-                              : const Color(0xffd3d3d3),
+        child: DefaultTextStyle(
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              showDivider
+                  ? Divider(
+                      height: dividerHeight,
+                      thickness: dividerThickness,
+                      color: dividerColor,
+                    )
+                  : Container(),
+              Container(
+                padding: padding,
+                margin: margin,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    GestureDetector(
+                      behavior: HitTestBehavior.translucent,
+                      child: Padding(
+                          padding: const EdgeInsets.only(
+                            top: 8,
+                            bottom: 8,
+                            left: 24,
+                            right: 32,
+                          ),
+                          child: skipWidget ??
+                              Text(
+                                skipText,
+                                style: skipStyle ??
+                                    const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                    ),
+                              )),
+                      onTap: onSkipTap,
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Stack(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: getDotsList(),
+                            )
+                          ],
                         ),
                       ),
                     ),
-                  ),
-                  GestureDetector(
-                    behavior: HitTestBehavior.translucent,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          top: 8, bottom: 8, left: 32, right: 24),
-                      child: nextWidget ?? Text(rightText),
+                    GestureDetector(
+                      behavior: HitTestBehavior.translucent,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 8, bottom: 8, left: 32, right: 24),
+                        child: rightWidget ??
+                            Text(
+                                pageNumber == pagesCount ? doneText : rightText,
+                                style: rightStyle ??
+                                    const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                    )),
+                      ),
+                      onTap: pageNumber == pagesCount ? onDoneTap : onNext,
                     ),
-                    onTap: onNext,
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       );
+
+  List<Widget> getDotsList() {
+    final List<Widget> list = [];
+    for (int i = 1; i <= pagesCount; i++) {
+      list.add(Container(
+        width: dotWidth ?? 12,
+        height: dotHeight ?? 12,
+        margin: dotMargin ?? const EdgeInsets.symmetric(horizontal: 4),
+        decoration: BoxDecoration(
+          shape: dotShape ?? BoxShape.rectangle,
+          color: pageNumber == i
+              ? activeColor ?? Colors.blue
+              : defaultColor ?? Colors.grey.withOpacity(0.5),
+        ),
+      ));
+    }
+    return list;
+  }
 }
---
+++ lib/components/intro_screen/gf_intro_slide.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class GFIntroSlide extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container();
+}
