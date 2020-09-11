5094cb2897fb7d2a322b12be9a2008b32f98b034
srinivas <srinivas@droid5.com>
2020-09-11 13:58:18 +0530

    added three types full,half,rounded in GFIntroScreen

--- lib/components/intro_screen/gf_intro_screen.dart
+++ lib/components/intro_screen/gf_intro_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/components/intro_screen/gf__intro_bottom_navigation.dart';
 import 'package:getwidget/components/intro_screen/gf_intro_slide.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:getwidget/types/gf_intro_type.dart';
 
 class GFIntroScreen extends StatefulWidget {
   const GFIntroScreen(
-      {Key key, this.slides, this.pageController, this.gfIntroBottomNavigation})
+      {Key key,
+      this.slides,
+      this.pageController,
+      this.gfIntroBottomNavigation,
+      this.type})
       : super(key: key);
 
   final List<Widget> slides;
+  final GFIntroType type;
   final PageController pageController;
   final GFIntroBottomNavigation gfIntroBottomNavigation;
 
@@ -35,25 +42,49 @@ class _GFIntroScreenState extends State<GFIntroScreen> {
   }
 
   @override
-  Widget build(BuildContext context) => Container(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-                child: PageView(
-              controller: _pageController,
-              children: widget.slides ?? slides(),
-            )),
-            widget.gfIntroBottomNavigation ??
-                GFIntroBottomNavigation(
-                  onNext: () {
-                    _pageController.nextPage(
-                        duration: const Duration(milliseconds: 500),
-                        curve: Curves.linear);
-                  },
-                  pagesCount: widget.slides ?? slides().length,
-                  pageNumber: page,
-                )
-          ],
+  Widget build(BuildContext context) => Center(
+        child: Container(
+          width: widget.type == GFIntroType.fullWidth
+              ? MediaQuery.of(context).size.width
+              : MediaQuery.of(context).size.width * 0.885,
+          height: widget.type != GFIntroType.fullWidth
+              ? MediaQuery.of(context).size.height / 2
+              : MediaQuery.of(context).size.height,
+          margin: widget.type != GFIntroType.fullWidth
+              ? const EdgeInsets.only(left: 20, right: 20)
+              : const EdgeInsets.only(left: 0, right: 0),
+          padding: widget.type == GFIntroType.fullWidth
+              ? const EdgeInsets.all(0)
+              : const EdgeInsets.all(0),
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            borderRadius: widget.type == GFIntroType.fullWidth
+                ? BorderRadius.circular(0)
+                : widget.type == GFIntroType.rounded
+                    ? BorderRadius.circular(20)
+                    : BorderRadius.zero,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                  child: PageView(
+                controller: _pageController,
+                children: widget.slides ?? slides(),
+              )),
+              widget.gfIntroBottomNavigation ??
+                  GFIntroBottomNavigation(
+                    onNext: () {
+                      _pageController.nextPage(
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.linear);
+                    },
+                    pagesCount: widget.slides ?? slides().length,
+                    pageNumber: page,
+                  )
+            ],
+          ),
         ),
       );
 
