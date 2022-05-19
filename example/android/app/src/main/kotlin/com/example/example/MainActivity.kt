a8f8e10d5ccb2063aad5aadb37fdab56831c205b
Arunbalaji001 <36786319+Arunbalaji001@users.noreply.github.com>
2022-05-19 09:47:51 +0530

    Null check operator issue solved

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,12 +1,7 @@
-package com.example.example
+package com.ionicfirebaseapp.getwidget_example
 
-import androidx.annotation.NonNull;
+
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
 }
