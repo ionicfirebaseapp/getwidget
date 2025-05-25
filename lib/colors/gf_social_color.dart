72798327bdeb9e79526303152890e787392e0877
sajalAppsdevs <manpreceptor962@gmail.com>
2025-05-25 20:31:54 +0600

    gf social color extend.

--- lib/colors/gf_social_color.dart
+++ lib/colors/gf_social_color.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
 
 class GFSocialColors {
+  // Meta & Messaging
   static const Color WHATSAPP = Color(0xff25D366);
   static const Color FACEBOOK = Color(0xff4267B2);
+  static const Color MESSENGER = Color(0xff0084FF);
+  static const Color TELEGRAM = Color(0xff0088cc);
+  static const Color WECHAT = Color(0xff09B83E);
+  static const Color LINE = Color(0xff00B900);
+  static const Color DISCORD = Color(0xff5865F2);
+  static const Color SIGNAL = Color(0xff3A76F0);
+
+  // Google Ecosystem
   static const Color GOOGLE = Color(0xffDB4437);
-  static const Color TWITTER = Color(0xff1DA1F2);
   static const Color YOUTUBE = Color(0xffFF0000);
+  static const Color GMAIL = Color(0xffD14836);
+  static const Color GOOGLE_DRIVE = Color(0xff0F9D58);
+  static const Color GOOGLE_MAPS = Color(0xff4285F4);
+
+  // Microblogging & Forums
+  static const Color TWITTER = Color(0xff1DA1F2);
+  static const Color REDDIT = Color(0xffFF4500);
+  static const Color TUMBLR = Color(0xff35465C);
+  static const Color BLUESKY = Color(0xff0066FF);
+
+  // Professional & Creative
   static const Color LINKEDIN = Color(0xff0077b5);
   static const Color DRIBBLE = Color(0xffea4c89);
-  static const Color PININTEREST = Color(0xffE60023);
+  static const Color BEHANCE = Color(0xff1769ff);
+  static const Color MEDIUM = Color(0xff00ab6c);
+  static const Color GITHUB = Color(0xff333333);
+  static const Color STACKOVERFLOW = Color(0xfff48024);
+  static const Color DEVTO = Color(0xff0A0A0A);
+
+  // Visual & Entertainment
+  static const Color PINTEREST = Color(0xffE60023);
+  static const Color TIKTOK = Color(0xff010101);
+  static const Color SNAPCHAT = Color(0xffFFFC00);
+  static const Color TWITCH = Color(0xff6441A4);
+  static const Color SPOTIFY = Color(0xff1DB954);
+  static const Color NETFLIX = Color(0xffE50914);
+
+  // Workspace Tools
   static const Color SLACK = Color(0xff2EB67D);
-  static const Color LINE = Color(0xff00B900);
-  static const Color WECHAT = Color(0xff09B83E);
-  static const Color TRANSPARENT = Colors.transparent;
+  static const Color ZOOM = Color(0xff2D8CFF);
+  static const Color SKYPE = Color(0xff00AFF0);
+  static const Color MICROSOFT_TEAMS = Color(0xff464EB8);
 }
